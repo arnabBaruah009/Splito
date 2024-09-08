28040ce64a54e805690c6e9f60b0976031391494
arnabbaruah009 <baruaharnab769@gmail.com>
2024-09-08 21:04:56 +0530

    login page added

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,11 +4,13 @@ import Layout from "../utils/Layout";
 import Dashboard from "../pages/Dashboard";
 import Groups from "../pages/Groups";
 import Friends from "../pages/Friends";
+import Login from "../pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Layout />}>
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="friends" element={<Friends />} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,26 @@
 const { nextui } = require("@nextui-org/react");
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-green": "#55efc4",
+        "custom-black": "#000000",
+      },
+      backgroundImage: {
+        "custom-gradient": "linear-gradient(315deg, #55efc4 0%, #000000 74%)",
+      },
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+        josefin: ["Josefin Sans", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [nextui()],
 };
