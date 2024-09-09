fa14181a8318aed8d43930fb59f2f91266fa59ec
Arnab Baruah <baruaharnab769@gmail.com>
2024-09-09 11:56:19 +0530

    dynamic values in TotalSummary component

--- src/components/Dashboard/TotalSummary.tsx
+++ src/components/Dashboard/TotalSummary.tsx
@@ -1,4 +1,7 @@
+import { useUser } from "../../hooks/useUser";
+
 const TotalSummary = () => {
+  const { user } = useUser();
   return (
     <div className="bg-white p-6 rounded-lg shadow-xl">
       <p>Total summary</p>
@@ -6,19 +9,19 @@ const TotalSummary = () => {
         <div className="w-full border-r-1">
           <div className="w-max">
             <p>Total amount you owe</p>
-            <p>&#8377; 10000.00</p>
+            <p>&#8377; {user?.totalBorrowed || 0}</p>
           </div>
         </div>
         <div className="w-full border-r-1">
           <div className="w-max mx-auto">
             <p>Total amount owe to you</p>
-            <p>&#8377; 10000.00</p>
+            <p>&#8377; {user?.totalLent || 0}</p>
           </div>
         </div>
         <div className="w-full">
           <div className="w-max mx-auto">
             <p>Total outstanding balance</p>
-            <p>&#8377; 10000.00</p>
+            <p>&#8377; {((user?.totalBorrowed || 0) - (user?.totalLent || 0))}</p>
           </div>
         </div>
       </div>
