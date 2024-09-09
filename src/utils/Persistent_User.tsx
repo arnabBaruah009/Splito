638e9460e9351671b84019f1c1a8c8fcf252eb49
Arnab Baruah <baruaharnab769@gmail.com>
2024-09-09 11:51:33 +0530

    persistent user implemented

---
+++ src/utils/Persistent_User.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import Cookies from "universal-cookie";
+import { useUser } from "../hooks/useUser";
+
+const PersistentUser = () => {
+  const { user, setUserID } = useUser();
+  const navigate = useNavigate();
+  const cookies = new Cookies();
+  useEffect(() => {
+    const getUser = () => {
+      const userID = cookies.get("splito-500K-bSnjthd6R34VKoZS2B3");
+      if (!userID) {
+        return navigate("/login");
+      }
+      setUserID(userID);
+    };
+    !user && getUser();
+  }, []);
+  return <Outlet />;
+};
+
+export default PersistentUser;
