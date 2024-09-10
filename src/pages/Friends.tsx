eda9191e46d18a5e382cd11a5925d8b46d6b9ced
Arnab Baruah <baruaharnab769@gmail.com>
2024-09-11 00:24:13 +0530

    modal added in friends and groups page

--- src/pages/Friends.tsx
+++ src/pages/Friends.tsx
@@ -1,5 +1,33 @@
+import { useDisclosure } from "@nextui-org/react";
+import Group from "../components/Groups/Group";
+import FRIEND_MODAL from "../components/Friends/Friends_Modal";
+
 const Friends = () => {
-  return <div>Friends</div>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  console.log("Friends component`");
+
+  return (
+    <div className="w-full h-full overflow-y-auto">
+      <div className="flex justify-between items-center mb-4">
+        {" "}
+        <h1 className="">Friends</h1>{" "}
+        <button
+          type="button"
+          onClick={onOpen}
+          className="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-1 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
+        >
+          + Add Friend
+        </button>
+      </div>
+
+      <div className="grid grid-cols-2 grid-rows-auto gap-6">
+        <Group />
+        <Group />
+        <Group />
+      </div>
+      <FRIEND_MODAL isOpen={isOpen} onClose={onClose} />
+    </div>
+  );
 };
 
 export default Friends;
--- src/pages/Groups.tsx
+++ src/pages/Groups.tsx
@@ -1,14 +1,29 @@
+import { useDisclosure } from "@nextui-org/react";
 import Group from "../components/Groups/Group";
+import GROUP_MODAL from "../components/Groups/Group_Modal";
 
 const Groups = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <div className="w-full h-full overflow-y-auto">
-      <h1 className="mb-4">Groups</h1>
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="">Groups</h1>
+        <button
+          type="button"
+          onClick={onOpen}
+          className="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-1 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
+        >
+          + Add Group
+        </button>
+      </div>
+
       <div className="grid grid-cols-2 grid-rows-auto gap-6">
         <Group />
         <Group />
         <Group />
       </div>
+      <GROUP_MODAL isOpen={isOpen} onClose={onClose} />
     </div>
   );
 };
