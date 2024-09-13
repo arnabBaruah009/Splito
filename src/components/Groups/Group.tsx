b61b5d618ef14565cd1713619d8b2af344bab114
Arnab Baruah <baruaharnab769@gmail.com>
2024-09-14 02:10:29 +0530

    group component refactored to dynamic data

--- src/components/Groups/Group.tsx
+++ src/components/Groups/Group.tsx
@@ -1,16 +1,19 @@
-import { Avatar } from "@nextui-org/react";
+import { Chip } from "@nextui-org/react";
 import { Link } from "react-router-dom";
 import Activity from "./Activity";
 import { GroupInfo } from "../../types";
 import { useEffect, useState } from "react";
 import { doc, onSnapshot } from "firebase/firestore";
 import { db } from "../../firebase";
+import { useUser } from "../../hooks/useUser";
 
 const Group = ({ id }: { id: string }) => {
+  const { user } = useUser();
+  const [overallAmt, setoverallAmt] = useState<number>(0);
   const [groupInfo, setGroupInfo] = useState<GroupInfo>({
     groupName: "",
     createdBy: "",
-    summary: [],
+    summary: {},
     groupSpending: 0,
     members: [],
     totalSpent: 0,
@@ -19,25 +22,35 @@ const Group = ({ id }: { id: string }) => {
   useEffect(() => {
     const unsub = onSnapshot(doc(db, "groups", id), (group) => {
       group.exists() && setGroupInfo(group.data() as GroupInfo);
+      setoverallAmt(
+        (groupInfo.summary?.[user?.email || ""]?.totalPaid || 0) -
+          (groupInfo.summary?.[user?.email || ""]?.totalShare || 0)
+      );
     });
 
     return unsub;
   }, []);
-  console.log("Group comp rendered");
 
   return (
     <div className="w-full rounded-lg shadow-xl bg-white p-8">
       <div className="flex justify-between">
         <div className="flex items-center">
-          <Avatar
-            size="md"
-            src="https://i.pravatar.cc/150?u=a04258114e29026702d"
-          />
           <p className="mx-4">{groupInfo.groupName}</p>
         </div>
         <div className="text-left">
-          <p>You are owed</p>
-          <p>&#8377; 3583.00</p>
+          {overallAmt === 0 ? (
+            <p className="text-green-500">All settled!!</p>
+          ) : overallAmt < 0 ? (
+            <p className="text-red-500">You owe</p>
+          ) : (
+            <p className="text-green-500">You are owed</p>
+          )}
+          <Chip
+            color={overallAmt < 0 ? "danger" : "success"}
+            variant="bordered"
+          >
+            &#8377; {overallAmt}
+          </Chip>
         </div>
       </div>
       <div>
@@ -45,7 +58,7 @@ const Group = ({ id }: { id: string }) => {
       </div>
       <div className="text-left pl-4 max-w-max">
         <Link
-          to="/user/friends"
+          to={`/user/groups/${id}`}
           className="text-slate-500 hover:text-slate-400 cursor-pointe"
         >
           View all
