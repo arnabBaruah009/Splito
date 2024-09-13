638ec69c53564c87b1243be52038ba455e9716b6
arnabbaruah009 <baruaharnab769@gmail.com>
2024-09-14 05:28:35 +0530

    add member list and implemented add expense modal

--- src/pages/Detailed_Group.tsx
+++ src/pages/Detailed_Group.tsx
@@ -1,15 +1,22 @@
-import { Chip } from "@nextui-org/react";
-import { doc, onSnapshot } from "firebase/firestore";
+import { Chip, useDisclosure } from "@nextui-org/react";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase";
 import { useUser } from "../hooks/useUser";
-import { GroupInfo } from "../types";
+import { GroupInfo, UserInfo } from "../types";
+import EXPENSE_MODAL from "../components/Groups/AddExpense_Modal";
+
+type MemberInfo = {
+  [email: string]: UserInfo;
+};
 
 const Detailed_Group = () => {
   const { user } = useUser();
   const { id } = useParams();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [overallAmt, setoverallAmt] = useState<number>(0);
+  const [memberInfo, setMemberInfo] = useState<MemberInfo>({});
   const [groupInfo, setGroupInfo] = useState<GroupInfo>({
     groupName: "",
     createdBy: "",
@@ -26,6 +33,29 @@ const Detailed_Group = () => {
 
     return unsub;
   }, []);
+
+  useEffect(() => {
+    setoverallAmt(
+      (groupInfo.summary[user?.email || ""]?.totalPaid || 0) -
+        groupInfo.summary[user?.email || ""]?.totalShare || 0
+    );
+  }, [groupInfo.summary]);
+
+  useEffect(() => {
+    const getMemberInfo = async () => {
+      const members = groupInfo.members;
+      const memberInfo: MemberInfo = {};
+      for (const member of members) {
+        const docRef = doc(db, "users", member);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          memberInfo[member] = docSnap.data() as UserInfo;
+        }
+      }
+      setMemberInfo(memberInfo);
+    };
+    getMemberInfo();
+  }, [groupInfo.members]);
 
   return (
     <div className="w-full h-full overflow-y-auto">
@@ -45,10 +75,27 @@ const Detailed_Group = () => {
             color={overallAmt < 0 ? "danger" : "success"}
             variant="bordered"
           >
-            &#8377; {overallAmt}
+            &#8377; {Math.abs(overallAmt)}
           </Chip>
         </div>
+        <button
+          type="button"
+          onClick={onOpen}
+          className="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-1 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
+        >
+          + Add Expense
+        </button>
       </div>
+      <div>
+        {Object.keys(memberInfo)
+          .sort()
+          .map((member) => {
+            return <p key={member}>{memberInfo[member].displayName}</p>;
+          })}
+      </div>
+      {/* Activities */}
+      <div></div>
+      <EXPENSE_MODAL isOpen={isOpen} onClose={onClose} />
     </div>
   );
 };
